d1e59a50a88b11bb3f943c734470e8442428e092
Isabella Hochschild <ihochschild@gmail.com>
2022-04-24 18:53:44 -0400

    SA5

---
+++ src/components/app.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import {
+  BrowserRouter, Routes, Route, NavLink, useParams,
+} from 'react-router-dom';
+import Controls from './controls';
+import Counter from './counter';
+
+function About(props) {
+  return <div> All there is to know about me </div>;
+}
+function Welcome(props) {
+  return (
+    <div>
+      <h1>Welcome!</h1><Counter />
+      <Controls />
+    </div>
+  );
+}
+
+function Nav(props) {
+  return (
+    <nav>
+      <ul>
+        <li><NavLink to="/" exact>Home</NavLink></li>
+        <li><NavLink to="/about">About</NavLink></li>
+        <li><NavLink to="/test/id1">test id1</NavLink></li>
+        <li><NavLink to="/test/id2">test id2</NavLink></li>
+      </ul>
+    </nav>
+  );
+}
+
+function Test(props) {
+  const { id } = useParams();
+  return <div> ID: {id} </div>;
+}
+
+function FallBack(props) {
+  return <div>URL Not Found</div>;
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Welcome />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/counter" element={<Counter />} />
+          <Route path="/test/:id" element={<Test />} />
+          <Route path="*" element={<FallBack />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
